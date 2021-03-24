abb4f4ddec01dc4322adc74c8e938cdd1036f337
anjali411 <chourdiaanjali123@gmail.com>
2021-03-24 12:01:12 -0700

    Support cmath.rect in TS
    ghstack-source-id: ffd55c852569580a48afc8ba197f5bae6357c2aa
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/54608

--- torch/csrc/jit/runtime/register_prim_ops.cpp
+++ torch/csrc/jit/runtime/register_prim_ops.cpp
@@ -779,6 +779,16 @@ RegisterOperators reg(
      DEFINE_STRING_OP(aten::add, a + b, str),
      DEFINE_COMPARISON_OP_WITH_COMPLEX(aten::eq, a == b),
      DEFINE_COMPARISON_OP_WITH_COMPLEX(aten::ne, a != b),
+     DEFINE_GENERIC_OP(
+         aten::polar,
+         c10::polar(static_cast<double>(a), static_cast<double>(b)),
+         c10::polar(static_cast<double>(a), static_cast<double>(b)),
+         complex,
+         complex),
+     DEFINE_INT_FLOAT_OP(
+         aten::polar,
+         c10::polar(static_cast<double>(a), static_cast<double>(b)),
+         complex),
      DEFINE_COMPARISON_OP(aten::lt, a < b),
      DEFINE_COMPARISON_OP(aten::gt, a > b),
      DEFINE_COMPARISON_OP(aten::le, a <= b),
