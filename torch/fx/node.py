a4bb3997ec795cba99def80e81370727c3b888a7
Horace He <horacehe2007@yahoo.com>
2021-04-23 13:02:27 -0700

    Added skeleton of python dispatch key
    ghstack-source-id: c145ac17d857453f53017af60f21104fff12e4e0
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/56718

--- torch/fx/node.py
+++ torch/fx/node.py
@@ -9,8 +9,8 @@ from torch.fx.operator_schemas import normalize_function, normalize_module
 if TYPE_CHECKING:
     from .graph import Graph
 
-BaseArgumentTypes = Union[str, int, float, bool, torch.dtype, torch.Tensor]
-base_types = BaseArgumentTypes.__args__  # type: ignore[attr-defined]
+BaseArgumentTypes = Union[str, int, float, bool, torch.dtype, torch.Tensor, torch.device]
+base_types = BaseArgumentTypes.__args__  # type: ignore
 
 Target = Union[Callable[..., Any], str]
 
